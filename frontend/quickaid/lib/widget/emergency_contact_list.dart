b9fb42c1444fd57ea4f5ab01b0d1ce95a585f7c8
coderMoose <mistercodermoose@gmail.com>
2024-03-02 18:05:15 -0500

    Add an alert dialog when clicking on an emergency contact item

--- frontend/quickaid/lib/widget/emergency_contact_list.dart
+++ frontend/quickaid/lib/widget/emergency_contact_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quickaid/resources/emergency_contact.dart';
+import 'package:quickaid/widget/emergency_contact_list_item.dart';
 
 class EmergencyContactList extends StatefulWidget {
   final List<EmergencyContact> emergencyContacts;
@@ -23,11 +24,47 @@ class EmergencyContactListState extends State<EmergencyContactList> {
     return ListView.builder(
       itemCount: emergencyContacts.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(emergencyContacts[index].name),
-          subtitle: Text('Email: ${emergencyContacts[index].email}'),
+        var emergencyContact = emergencyContacts[index];
+
+        return GestureDetector (
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return EditEmergencyContactDialog(emergencyContact: emergencyContact);
+              },
+            );
+          },
+          child: EmergencyContactListItem(
+            emergencyContact: emergencyContact
+          )
         );
       },
+    );
+  }
+}
+
+class EditEmergencyContactDialog extends StatelessWidget {
+  const EditEmergencyContactDialog({
+    super.key,
+    required this.emergencyContact,
+  });
+
+  final EmergencyContact emergencyContact;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Emergency Contact Details'),
+      content: Text('Name: ${emergencyContact.name}\nPhone: ${emergencyContact.phoneNumber}'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Close the dialog
+          },
+          child: const Text('Close'),
+        ),
+      ],
     );
   }
 }
